75528e51753ecb7fb99f76d8c27caa151ea11bba
Pedro Antunes <pedro.antunes@bsurance.tech>
2021-07-03 15:15:44 +0100

    Change db settings

--- pharmago/settings.py
+++ pharmago/settings.py
@@ -79,24 +79,16 @@ WSGI_APPLICATION = "pharmago.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if "RDS_DB_NAME" in os.environ:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.postgresql",
-            "NAME": os.environ["RDS_DB_NAME"],
-            "USER": os.environ["RDS_USERNAME"],
-            "PASSWORD": os.environ["RDS_PASSWORD"],
-            "HOST": os.environ["RDS_HOSTNAME"],
-            "PORT": os.environ["RDS_PORT"],
-        }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "pharma",
+        "USER": "pharma",
+        "PASSWORD": "pharma",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-        }
-    }
+}
 
 
 # Password validation
